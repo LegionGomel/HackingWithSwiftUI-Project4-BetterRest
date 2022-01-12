000cae7888e3a524e112507810c45c0b59bd5d00
Yuri Ershov <shtorm0606@gmail.com>
2022-01-12 18:36:28 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Yuri Ershov on 12.01.22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
